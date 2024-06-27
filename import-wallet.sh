32365382b846bc7228a144c5f63e1b09cfe80f91
Motko <77466238+Motko222@users.noreply.github.com>
2024-06-28 00:09:15 +0200

    Create import-wallet.sh

---
+++ import-wallet.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
+source ~/scripts/$folder/cfg
+
+[ -z $1 ] && read -p "Key name ? " key || key=$1
+
+$BINARY keys add $key --eth --recover
+$BINARY keys show $key --bech val
+echo "0x$($BINARY debug addr $(echo $PASS | $BINARY keys show $key -a) | grep hex | awk '{print $3}')"

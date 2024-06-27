ce0a4714ff6b8e318b1fdf1adb9d29531a6f16c2
Motko <77466238+Motko222@users.noreply.github.com>
2024-06-27 23:52:29 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
-config=~/scripts/$folder/cfg
 
 #install binary
 cd ~
@@ -9,6 +8,9 @@ git clone -b v0.2.3 https://github.com/0glabs/0g-chain.git
 ./0g-chain/networks/testnet/install.sh
 source ~/.bash_profile
 
-$BINARY version
-
+#create cfg file
 cp ~/scripts/$folder/cfg.sample ~/scripts/$folder/cfg
+source ~/scripts/$folder/cfg
+
+#check version
+$BINARY version

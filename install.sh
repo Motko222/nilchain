55995c90d884998842343f524ae02fd64cfb9ee7
Motko <77466238+Motko222@users.noreply.github.com>
2024-06-27 23:20:04 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,11 @@ folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{prin
 config=~/scripts/$folder/cfg
 
 #install binary
-#put instalation script here
+cd ~
+git clone -b v0.2.3 https://github.com/0glabs/0g-chain.git
+./0g-chain/networks/testnet/install.sh
+source ~/.bash_profile
 
 $BINARY version
+
+cp ~/scripts/$folder/cfg.sample ~/scripts/$folder/cfg

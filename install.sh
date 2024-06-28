50e2fc00577f6c99a7d990c192dfa02271e1d66f
Motko <77466238+Motko222@users.noreply.github.com>
2024-06-28 22:51:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
 
+#remove old binary and repo
+rm $(which 0gchaind)
+rm -r ~/0g-chain
+
 #install binary
 cd ~
 git clone -b v0.2.3 https://github.com/0glabs/0g-chain.git
@@ -9,9 +13,13 @@ git clone -b v0.2.3 https://github.com/0glabs/0g-chain.git
 source ~/.bash_profile
 
 #create cfg file
-cp ~/scripts/$folder/cfg.sample ~/scripts/$folder/cfg
-source ~/scripts/$folder/cfg
-nano ~/scripts/$folder/cfg
+if [ -f ~/scripts/$folder/cfg ]
+then
+ echo "Config exists."
+else
+ cp ~/scripts/$folder/cfg.sample ~/scripts/$folder/cfg
+ nano ~/scripts/$folder/cfg
+fi
 
 #check version
 $BINARY version

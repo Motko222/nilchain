ff15a74a8b276721b2f8d206f5f93fb595f2816b
Motko <77466238+Motko222@users.noreply.github.com>
2024-06-28 11:04:03 +0200

    Create wallet-summary.sh

---
+++ wallet-summary.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
+source ~/scripts/$folder/config/env
+source ~/.bash_profile
+
+echo   "---- SUMMARY --------------------------------------------------------------------------"
+printf "%-12s %9s %9s %9s\n" Id Balance Delegated Reward
+echo   "---------------------------------------------------------------------------------------"
+
+keys=$(echo $PASS | $BINARY keys list | grep -E 'name' | sed 's/  name: //g')
+
+echo $keys | while read line
+do
+   key=$(echo $line | awk '{print $1}')
+   wallet=$(echo $PASS | $BINARY keys show $key -a) 
+   balance=$($BINARY query bank balances $wallet | grep amount | awk '{print $3}' | sed 's/"//g' | awk '{print $1/1000000}')
+   valoper=$(echo $PASS | $BINARY keys show $KEY --bech val | grep valoper | awk '{print $3}')
+   rewards=$($BINARY query distribution rewards $wallet $valoper 2>/dev/null \
+     | grep amount | awk '{print $3}' | sed 's/"//g' | awk '{print $1/1000000}')
+
+   stake=$($BINARY query staking delegation $wallet $valoper 2>/dev/null \
+     | grep amount | awk '{print $2}' | sed 's/"//g' | awk '{print $1/1000000}' )
+
+   printf "%-12s %9s %9s %9s\n" \
+      $key $balance $stake $rewards
+done

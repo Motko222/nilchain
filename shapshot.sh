59fed546326c2fc0b34728fbbe5463ac65c812a1
Motko <77466238+Motko222@users.noreply.github.com>
2024-07-24 11:29:54 +0200

    Create shapshot.sh

---
+++ shapshot.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
+source ~/scripts/$folder/cfg
+source ~/.bash_profile
+
+sudo systemctl stop $BINARY.service
+sudo journalctl -u $BINARY.service -f --no-hostname -o cat
+
+sudo systemctl stop 0gchaind
+
+cp $HOME/.0gchain/data/priv_validator_state.json $HOME/.0gchain/priv_validator_state.json.backup
+
+0gchaind tendermint unsafe-reset-all --home $HOME/.0gchain --keep-addr-book
+curl https://snapshots-testnet.unitynodes.com/0gchain-testnet/0gchain-testnet-latest.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.0gchain
+
+mv $HOME/.0gchain/priv_validator_state.json.backup $HOME/.0gchain/data/priv_validator_state.json
+
+sudo systemctl start $BINARY.service
+sudo journalctl -u $BINARY.service -f --no-hostname -o cat

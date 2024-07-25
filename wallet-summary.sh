ed637a39bacf220894bc8fc7b02bd662be24cc64
Motko <77466238+Motko222@users.noreply.github.com>
2024-07-25 10:18:57 +0200

    Update wallet-summary.sh

--- wallet-summary.sh
+++ wallet-summary.sh
@@ -9,10 +9,12 @@ printf "%-12s %9s %9s %9s %9s\n" Id Balance Delegated Reward Da
 echo   "---------------------------------------------------------------------------------------"
 
 keys=$(echo $PASS | $BINARY keys list | grep -E 'name' | sed 's/  name: //g')
+keys=main
 
 for key in $keys
 do
    wallet=$(echo $PASS | $BINARY keys show $key -a) 
+   wallet_eth="0x$($BINARY debug addr $(echo $PASS | $BINARY keys show $KEY -a) | grep hex | awk '{print $3}')"
    balance=$($BINARY query bank balances $wallet | grep amount | awk '{print $3}' | sed 's/"//g' | awk '{print $1/1000000}')
    [ -z $balance ] && balance="-"
    valoper=$(echo $PASS | $BINARY keys show $KEY --bech val | grep valoper | awk '{print $3}')
@@ -23,7 +25,7 @@ do
      | grep amount | awk '{print $2}' | sed 's/"//g' | awk '{print $1/1000000}' )
    [ -z $stake ] && stake="-"
 
-   da=$(curl -sX 'GET'   'https://chainscan-newton.0g.ai/api/v2/addresses/'$wallet'/transactions?filter=to%20%7C%20from'   -H 'accept: application/json' | jq | grep -c 0x0000000000000000000000000000000000001000)
+   da=$(curl -sX 'GET'   'https://chainscan-newton.0g.ai/api/v2/addresses/'$wallet_eth'/transactions?filter=to%20%7C%20from'   -H 'accept: application/json' | jq | grep -c 0x0000000000000000000000000000000000001000)
 
    printf "%-12s %9s %9s %9s %9s\n" \
       $key $balance $stake $rewards $da

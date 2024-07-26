f7723a0a53a945d1eb1749eb2ea0a2a536902168
Motko <77466238+Motko222@users.noreply.github.com>
2024-07-26 15:01:02 +0200

    Update import-wallet.sh

--- import-wallet.sh
+++ import-wallet.sh
@@ -8,3 +8,4 @@ source ~/scripts/$folder/cfg
 $BINARY keys add $key --eth --recover
 $BINARY keys show $key --bech val
 echo "0x$($BINARY debug addr $(echo $PASS | $BINARY keys show $key -a) | grep hex | awk '{print $3}')"
+{ echo y; sleep 1; echo $PASS; } |  $BINARY keys unsafe-export-eth-key $key

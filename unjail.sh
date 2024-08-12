59573344d0a2a17abf0ef896ed62c2558bda12f2
Motko <77466238+Motko222@users.noreply.github.com>
2024-08-12 18:07:56 +0200

    Update unjail.sh

--- unjail.sh
+++ unjail.sh
@@ -2,5 +2,6 @@
 
 folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
 source ~/scripts/$folder/cfg
+source ~/.bash_profile
 
 echo $PASS | $BINARY tx slashing unjail --from $KEY --gas-adjustment $GAS_ADJ --gas auto -y

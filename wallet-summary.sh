44fc15f54b0295b6c637b806728ae2cbf64cd44c
Motko <77466238+Motko222@users.noreply.github.com>
2024-08-15 15:32:35 +0200

    Update wallet-summary.sh

--- wallet-summary.sh
+++ wallet-summary.sh
@@ -16,19 +16,23 @@ for key in $keys
 do
    wallet=$(echo $PASS | $BINARY keys show $key -a) 
    wallet_eth="0x$($BINARY debug addr $(echo $PASS | $BINARY keys show $key -a) | grep hex | awk '{print $3}')"
-   balance=$($BINARY query bank balances $wallet | grep amount | awk '{print $3}' | sed 's/"//g' | awk '{print $1/1000000}')
+#   balance=$($BINARY query bank balances $wallet | grep amount | awk '{print $3}' | sed 's/"//g' | awk '{print $1/1000000}')
+   balance=$($BINARY query bank balances $wallet -o json | jq '.balances[] | select(.denom=="ua0gi")' | jq -r .amount | awk '{print $1/1000000}')
    [ -z $balance ] && balance="-"
-   valoper=$(echo $PASS | $BINARY keys show $KEY --bech val | grep valoper | awk '{print $3}')
-   rewards=$($BINARY query distribution rewards $wallet $valoper 2>/dev/null \
-     | grep amount | awk '{print $3}' | sed 's/"//g' | awk '{print $1/1000000}')
-   [ -z $rewards ] && rewards="-"
-   stake=$($BINARY query staking delegation $wallet $valoper 2>/dev/null \
-     | grep amount | awk '{print $2}' | sed 's/"//g' | awk '{print $1/1000000}' )
-   [ -z $stake ] && stake="-"
 
-   da=$(curl -sX 'GET'   'https://chainscan-newton.0g.ai/api/v2/addresses/'$wallet_eth'/transactions?filter=to%20%7C%20from'   -H 'accept: application/json' | jq | grep -c 0x0000000000000000000000000000000000001000)
-   uploads=$(curl -sX 'GET'   'https://chainscan-newton.0g.ai/api/v2/addresses/'$wallet_eth'/transactions?filter=to%20%7C%20from'   -H 'accept: application/json' | jq | grep -c -e 0x8873cc79c5b3b5666535C825205C9a128B1D75F1|0xB7e39604f47c0e4a6Ad092a281c1A8429c2440d3)
-   faucet=$(curl -sX 'GET'   'https://chainscan-newton.0g.ai/api/v2/addresses/'$wallet_eth'/transactions?filter=to%20%7C%20from'   -H 'accept: application/json' | jq | grep -c 0x83c4A688174A8d4b99b4C8A2feC124dff79D58d2)
+#   valoper=$(echo $PASS | $BINARY keys show $KEY --bech val | grep valoper | awk '{print $3}')
+#   rewards=$($BINARY query distribution rewards $wallet $valoper 2>/dev/null \
+#     | grep amount | awk '{print $3}' | sed 's/"//g' | awk '{print $1/1000000}')
+   [ -z $rewards ] && rewards="-"
+#   stake=$($BINARY query staking delegation $wallet $valoper 2>/dev/null \
+#     | grep amount | awk '{print $2}' | sed 's/"//g' | awk '{print $1/1000000}' )
+#   [ -z $stake ] && stake="-"
+
+   stake=$(0gchaind query staking delegations $wallet -o json | jq -r .delegation_responses[].delegation.shares | awk '{s+=$1} END {print s/1000000}')
+   [ -z $stake ] && stake="-"
+#   da=$(curl -sX 'GET'   'https://chainscan-newton.0g.ai/api/v2/addresses/'$wallet_eth'/transactions?filter=to%20%7C%20from'   -H 'accept: application/json' | jq | grep -c 0x00000000000000000000000000>
+#   uploads=$(curl -sX 'GET'   'https://chainscan-newton.0g.ai/api/v2/addresses/'$wallet_eth'/transactions?filter=to%20%7C%20from'   -H 'accept: application/json' | jq | grep -c -e 0x8873cc79c5b3b56665>
+#   faucet=$(curl -sX 'GET'   'https://chainscan-newton.0g.ai/api/v2/addresses/'$wallet_eth'/transactions?filter=to%20%7C%20from'   -H 'accept: application/json' | jq | grep -c 0x83c4A688174A8d4b99b4C8>
 
    printf "%-12s %-9s %-9s %-12s %-9s %-9s %-9s\n" \
       $key $balance $stake $rewards $da $uploads $faucet

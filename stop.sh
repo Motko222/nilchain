1005ad611476d1dd557da99c38693a4106f8ca4f
Motko <77466238+Motko222@users.noreply.github.com>
2024-09-06 21:48:55 +0200

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
-source ~/scripts/$folder/cfg
+path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
+folder=$(echo $path | awk -F/ '{print $NF}')
+source $path/cfg
 
 sudo systemctl stop $BINARY.service

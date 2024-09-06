340a5fd9b4e818e23b3915c70070af7bb212f5ca
Motko <77466238+Motko222@users.noreply.github.com>
2024-09-06 21:46:02 +0200

    Update import-wallet.sh

--- import-wallet.sh
+++ import-wallet.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
-source ~/scripts/$folder/cfg
+path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
+folder=$(echo $path | awk -F/ '{print $NF}')
+source $path/cfg
 
 [ -z $1 ] && read -p "Key name ? " key || key=$1
 

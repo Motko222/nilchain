c212922f813c15f0021493be165612e39eb5cfe1
Motko <77466238+Motko222@users.noreply.github.com>
2024-09-06 21:48:45 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
-source ~/scripts/$folder/cfg
+path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
+folder=$(echo $path | awk -F/ '{print $NF}')
+source $path/cfg
 
 sudo systemctl restart $BINARY.service
 sudo journalctl -u $BINARY.service -f --no-hostname -o cat

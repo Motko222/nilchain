f2deaf0a8000dcef3941985cc57993807b2da248
Motko <77466238+Motko222@users.noreply.github.com>
2024-09-06 21:44:04 +0200

    Update create-wallet.sh

--- create-wallet.sh
+++ create-wallet.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
-source ~/scripts/$folder/cfg
+path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
+folder=$(echo $path | awk -F/ '{print $NF}')
+source $path/config
 
 [ -z $1 ] && read -p "Key name ? " key || key=$1
 
-$BINARY keys add $key --eth
+$BINARY keys add $key
 $BINARY keys show $key --bech val
-echo "0x$($BINARY debug addr $(echo $PASS | $BINARY keys show $key -a) | grep hex | awk '{print $3}')"

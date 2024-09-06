85ab6fb495f1bc50cf89930f3e9934adc34504d9
Motko <77466238+Motko222@users.noreply.github.com>
2024-09-06 22:09:33 +0200

    Update wallet-summary.sh

--- wallet-summary.sh
+++ wallet-summary.sh
@@ -9,7 +9,7 @@ read -p "Keys? (blank for all) " keys
 [ -z $keys ] && keys=$(echo $PASS | $BINARY keys list | grep -E 'name' | sed 's/  name: //g')
 
 echo   "---- SUMMARY --------------------------------------------------------------------------"
-printf "%-12s %-9s %-9s %-12s %-9s %-9s %-9s\n" Id Balance Delegated Reward Da Uploads Faucet
+printf "%-12s %-9s %-9s %-12s \n" Id Balance Delegated Reward
 echo   "---------------------------------------------------------------------------------------"
 
 
@@ -27,14 +27,11 @@ do
    [ -z $rewards ] && rewards="-"
 #   stake=$($BINARY query staking delegation $wallet $valoper 2>/dev/null \
 #     | grep amount | awk '{print $2}' | sed 's/"//g' | awk '{print $1/1000000}' )
-#   [ -z $stake ] && stake="-"
+   [ -z $stake ] && stake="-"
 
    stake=$(0gchaind query staking delegations $wallet -o json | jq -r .delegation_responses[].delegation.shares | awk '{s+=$1} END {print s/1000000}')
    [ -z $stake ] && stake="-"
-#   da=$(curl -sX 'GET'   'https://chainscan-newton.0g.ai/api/v2/addresses/'$wallet_eth'/transactions?filter=to%20%7C%20from'   -H 'accept: application/json' | jq | grep -c 0x00000000000000000000000000>
-#   uploads=$(curl -sX 'GET'   'https://chainscan-newton.0g.ai/api/v2/addresses/'$wallet_eth'/transactions?filter=to%20%7C%20from'   -H 'accept: application/json' | jq | grep -c -e 0x8873cc79c5b3b56665>
-#   faucet=$(curl -sX 'GET'   'https://chainscan-newton.0g.ai/api/v2/addresses/'$wallet_eth'/transactions?filter=to%20%7C%20from'   -H 'accept: application/json' | jq | grep -c 0x83c4A688174A8d4b99b4C8>
 
-   printf "%-12s %-9s %-9s %-12s %-9s %-9s %-9s\n" \
-      $key $balance $stake $rewards $da $uploads $faucet
+   printf "%-12s %-9s %-9s %-12s \n" \
+      $key $balance $stake $rewards
 done

0d249e4302b5eb05ffea43941ab88f94a228b6ee
Motko <77466238+Motko222@users.noreply.github.com>
2024-11-21 12:51:05 +0100

    Update init-service.sh

--- init-service.sh
+++ init-service.sh
@@ -6,19 +6,20 @@ source $path/config
 
 sudo tee /etc/systemd/system/$folder.service > /dev/null << EOF
 [Unit]
-Description=$BINARY node service
+Description="nillion node"
 After=network-online.target
 
 [Service]
 User=root
-ExecStart=/root/go/bin/$BINARY start --log_output_console
-Restart=on-failure
-RestartSec=10
-LimitNOFILE=65535
-Environment="DAEMON_HOME=/root/.$BINARY"
-Environment="DAEMON_NAME=$BINARY"
+ExecStart=/root/go/bin/cosmovisor start
+Restart=always
+RestartSec=3
+LimitNOFILE=4096
+Environment="DAEMON_NAME=nilchaind"
+Environment="DAEMON_HOME=/root/.nillionapp"
+Environment="DAEMON_ALLOW_DOWNLOAD_BINARIES=false"
+Environment="DAEMON_RESTART_AFTER_UPGRADE=true"
 Environment="UNSAFE_SKIP_BACKUP=true"
-Environment="PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
 
 [Install]
 WantedBy=multi-user.target

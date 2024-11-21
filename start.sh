e881a451deeb4477a56e7e1569f400eca4a32068
Motko <77466238+Motko222@users.noreply.github.com>
2024-11-21 12:48:18 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,6 @@
 
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
-source $path/config
 
 sudo systemctl restart $folder.service
 sudo journalctl -u $folder.service -f --no-hostname -o cat

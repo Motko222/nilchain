b4705385433902b8e42a803cf72e5c44bbde4613
Motko <77466238+Motko222@users.noreply.github.com>
2024-11-21 12:45:19 +0100

    Update report.sh

--- report.sh
+++ report.sh
@@ -2,7 +2,7 @@
 
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
-source $path/cfg
+source $path/config
 source ~/.bash_profile
 json=~/logs/report-$folder
 

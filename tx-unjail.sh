7877b97bc055a73e1d90624dcdee760b96c1441d
Motko <77466238+Motko222@users.noreply.github.com>
2024-11-21 12:46:08 +0100

    Update tx-unjail.sh

--- tx-unjail.sh
+++ tx-unjail.sh
@@ -2,7 +2,7 @@
 
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
-source $path/cfg
+source $path/config
 source ~/.bash_profile
 
 echo $PASS | $BINARY tx slashing unjail --from $KEY --gas-adjustment $GAS_ADJ --gas auto -y

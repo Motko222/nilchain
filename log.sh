d6f398ce6799ba49a1024be12e4cb1c6126664b6
Motko <77466238+Motko222@users.noreply.github.com>
2024-11-21 12:48:36 +0100

    Update log.sh

--- log.sh
+++ log.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
-source $path/config
 
-sudo journalctl -u $BINARY.service -f --no-hostname -o cat
+sudo journalctl -u $folder.service -f --no-hostname -o cat

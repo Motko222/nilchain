1967d5ab38ffdbb0e4de4cfc780cbe3189398165
Motko <77466238+Motko222@users.noreply.github.com>
2024-11-21 12:54:07 +0100

    Update init-install.sh

--- init-install.sh
+++ init-install.sh
@@ -3,22 +3,13 @@
 folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
 source ~/.bash_profile
 
-read -p "Tag? " tag
-
-#remove old binary and repo
-rm $(which 0gchaind)
-rm -r ~/0g-chain
-
-#install binary
-git clone TBD nillion
-cd nillion
-git checkout $tag
-make install
-
-# cosmovisor
+# cosmovisor setup
 mkdir -p ~/.nillionapp/cosmovisor/genesis/bin
 mkdir -p ~/.nillionapp/cosmovisor/upgrades
 cp ~/go/bin/nilchaind ~/.nillionapp/cosmovisor/genesis/bin
+
+#download binary
+wget -O $HOME/.nillionapp/cosmovisor/genesis/bin/nilchaind https://snapshots.kjnodes.com/nillion-testnet/nilchaind-v0.2.2-linux-amd64
 
 #create config file
 if [ -f ~/scripts/$folder/config ]

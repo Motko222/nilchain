50c279fc5df7060fd6b475b2713eb5de42adb027
Motko <77466238+Motko222@users.noreply.github.com>
2024-12-09 22:10:57 +0100

    Update install-binary.sh

--- install-binary.sh
+++ install-binary.sh
@@ -2,28 +2,25 @@
 
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
-source $path/config
 source ~/.bash_profile
 
-# cosmovisor setup
-mkdir -p /root/.nillionapp/cosmovisor/genesis/bin
-mkdir -p /root/.nillionapp/cosmovisor/upgrades
-cp ~/go/bin/nilchaind /root/.nillionapp/cosmovisor/genesis/bin
-
-#download binary
-wget -O /root/.nillionapp/cosmovisor/genesis/bin/nilchaind https://snapshots.kjnodes.com/nillion-testnet/nilchaind-v0.2.2-linux-amd64
-chmod +x /root/.nillionapp/cosmovisor/genesis/bin/nilchaind
-
 #create config file
-if [ -f ~/scripts/$folder/config ]
+if [ -f $path/config ]
 then
  echo "Config exists."
 else
- cp /root/scripts/$folder/config.sample ~/scripts/$folder/config
- nano /root/scripts/$folder/config
+ cp $path/config.sample $path/config
+ nano $path/config
 fi
+source $path/config
 
-source /root/scripts/$folder/config
+# cosmovisor setup
+mkdir -p $DATA/cosmovisor/genesis/bin
+mkdir -p $DATA/cosmovisor/upgrades
+
+#download binary
+wget -O $DATA/cosmovisor/genesis/bin/$BINARY https://snapshots.kjnodes.com/nillion-testnet/nilchaind-v0.2.2-linux-amd64
+chmod +x $DATA/cosmovisor/genesis/bin/$BINARY
 
 #check version
 $BINARY version

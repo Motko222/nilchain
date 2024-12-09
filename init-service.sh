397ef81a2b1468616afc9d7b18364bd9b6696608
Motko <77466238+Motko222@users.noreply.github.com>
2024-12-09 21:59:01 +0100

    Update init-service.sh

--- init-service.sh
+++ init-service.sh
@@ -6,17 +6,17 @@ source $path/config
 
 sudo tee /etc/systemd/system/$folder.service > /dev/null << EOF
 [Unit]
-Description="nillion node"
+Description="$folder"
 After=network-online.target
 
 [Service]
 User=root
 ExecStart=/root/go/bin/cosmovisor start
 Restart=always
-RestartSec=3
+RestartSec=30
 LimitNOFILE=4096
-Environment="DAEMON_NAME=nilchaind"
-Environment="DAEMON_HOME=/root/.nillionapp"
+Environment="DAEMON_NAME=$BINARY"
+Environment="DAEMON_HOME=$DATA"
 Environment="DAEMON_ALLOW_DOWNLOAD_BINARIES=false"
 Environment="DAEMON_RESTART_AFTER_UPGRADE=true"
 Environment="UNSAFE_SKIP_BACKUP=true"
